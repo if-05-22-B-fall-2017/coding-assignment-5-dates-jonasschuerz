2bd87b13d307aa47cb979b57ee0a544af37f2057
jonasschuerz <32167805+jonasschuerz@users.noreply.github.com>
2018-04-25 22:33:31 +0200

    Update GermanStyleDateChecker.java

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,6 +3,12 @@ import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
     public static Matcher getMatcher(String dateString) {
-        return null;
+          return Pattern.compile("^"
+                + "(([0-2]?[0-9])|(3[0-1]))"
+                + "[\\s.-]"
+                + "((0?[1-9])|(1[0-2]))"
+                + "[\\s.-]"
+                + "[1-2]?(0|9)?[0-9]{2}"
+                + "$").matcher(dateString);
     }
 }
